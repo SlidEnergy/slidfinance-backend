453b116e2fc8c7fa583a6958c19f22fb76a5b720
SlidEnergy <slidenergy@gmail.com>
2019-01-06 22:01:31 +1000

    disable validate audience and issuer, change ajax post, add migrations

--- MyFinanceServer/Startup.cs
+++ MyFinanceServer/Startup.cs
@@ -75,16 +75,16 @@ namespace MyFinanceServer
                         options.TokenValidationParameters = new TokenValidationParameters
                         {
                             // укзывает, будет ли валидироваться издатель при валидации токена
-                            ValidateIssuer = true,
+                            ValidateIssuer = false,
                             // строка, представляющая издателя
                             //ValidIssuer = AuthOptions.ISSUER,
 
                             // будет ли валидироваться потребитель токена
-                            ValidateAudience = true,
+                            ValidateAudience = false,
                             // установка потребителя токена
                             //ValidAudience = AuthOptions.AUDIENCE,
                             // будет ли валидироваться время существования
-                            ValidateLifetime = true,
+                            //ValidateLifetime = true,
 
                             // установка ключа безопасности
                             IssuerSigningKey = new SymmetricSecurityKey(key),
