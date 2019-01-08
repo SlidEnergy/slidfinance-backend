c86efac806fb1e5175142d7bac085d4deca5412a
SlidEnergy <slidenergy@gmail.com>
2019-01-09 00:26:36 +1000

    add transaction sketch

---
+++ MyFinanceServer/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFinanceServer.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public float Amount { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public string BankDescription { get; set; }
+
+        public string BankCategory { get; set; }
+    }
+}
