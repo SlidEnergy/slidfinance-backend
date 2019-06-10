0618376af856165761bd4e85bf12a9aa32404a28
kluchko <kluchko@lers.ru>
2019-06-10 15:46:23 +1000

    feat: Добавил связь правила и аккаунта

---
+++ MyFinanceServer/Migrations/20190610054525_ruleAccountLink.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFinanceServer.Migrations
+{
+    public partial class ruleAccountLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rules_Accounts_AccountId",
+                table: "Rules");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "Rules",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rules_Accounts_AccountId",
+                table: "Rules",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rules_Accounts_AccountId",
+                table: "Rules");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "Rules",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rules_Accounts_AccountId",
+                table: "Rules",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
