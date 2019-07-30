9aba7b42f4feea8d322dfd8a1b1627ce57785130
kluchko <kluchko@lers.ru>
2019-07-30 16:22:07 +1000

    chore: Удален неиспользуемый файл

--- SlidFinance.WebApi/Startup.cs
+++ SlidFinance.WebApi/Startup.cs
@@ -57,8 +57,6 @@ namespace SlidFinance.WebApi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-
-
 			services
 				.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 				.AddJwtBearer(options =>
