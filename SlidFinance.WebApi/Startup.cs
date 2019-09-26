2f319b6a0f5bf6b44f1216bed33dfc9004df84b7
kluchko <kluchko@lers.ru>
2019-09-26 16:36:29 +1000

    refactor: Разделил метод на несколько

--- SlidFinance.WebApi/Startup.cs
+++ SlidFinance.WebApi/Startup.cs
@@ -22,32 +22,6 @@ namespace SlidFinance.WebApi
 		public IConfiguration Configuration { get; }
 		private IHostingEnvironment CurrentEnvironment { get; }
 
-		private string ConnectionString => ConnectionStringFactory.Get();
-
-		private AuthSettings AuthSettings
-		{
-			get
-			{
-				if (CurrentEnvironment.IsDevelopment())
-				{
-					return Configuration
-						.GetSection("Security")
-						.GetSection("Token")
-						.Get<AuthSettings>();
-				}
-				else
-				{
-					return new AuthSettings
-					{
-						Audience = Environment.GetEnvironmentVariable("TOKEN_AUDIENCE"),
-						Issuer = Environment.GetEnvironmentVariable("TOKEN_ISSUER"),
-						Key = Environment.GetEnvironmentVariable("TOKEN_KEY"),
-						LifetimeMinutes = Convert.ToInt32(Environment.GetEnvironmentVariable("TOKEN_LIFETIME_MINUTES"))
-					};
-				}
-			}
-		}
-
 		public Startup(IConfiguration configuration, IHostingEnvironment env)
 		{
 			Configuration = configuration;
@@ -57,96 +31,20 @@ namespace SlidFinance.WebApi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services
-				.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options =>
-				{
-					options.RequireHttpsMetadata = false;
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						// Укзывает, будет ли проверяться издатель при проверке токена
-						ValidateIssuer = false,
-						// Строка, представляющая издателя
-						ValidIssuer = AuthSettings.Issuer,
+			ConfigureAuthorization(services);
 
-						// Будет ли проверяться потребитель токена
-						ValidateAudience = false,
-						// Установка потребителя токена
-						ValidAudience = AuthSettings.Audience,
-						// будет ли валидироваться время существования
-						ValidateLifetime = true,
-
-						// установка ключа безопасности
-						IssuerSigningKey = AuthSettings.GetSymmetricSecurityKey(),
-						// валидация ключа безопасности
-						ValidateIssuerSigningKey = true,
-					};
-					// options.SaveToken = true;
-				});
-
-			// AutoMapper
-			services.AddScoped(provider => new MapperConfiguration(cfg =>
-			{
-				cfg.AddProfile(new MappingProfile(provider.GetService<ApplicationDbContext>()));
-			}).CreateMapper());
+			ConfigureAutoMapper(services);
 
 			services.AddCors();
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-			services.AddEntityFrameworkNpgsql()
-				.AddDbContext<ApplicationDbContext>(options => options
-					.UseLazyLoadingProxies()
-					.UseNpgsql(ConnectionString))
-				.BuildServiceProvider();
+			ConfigureDataAccess(services);
 
-			// Register the Swagger generator, defining 1 or more Swagger documents
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new Info { Title = "SlidFinance", Version = "v1" });
-				c.AddSecurityDefinition("Oauth2", new OAuth2Scheme
-				{
-					Type = "oauth2",
-					Flow = "implicit",
-					TokenUrl = "/api/v1/tokens"
-				});
-				c.AddSecurityDefinition("Bearer", new ApiKeyScheme { In = "header", Description = "Please enter JWT with Bearer into field", Name = "Authorization", Type = "apiKey" });
-				c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
-					{ "Oauth2", Enumerable.Empty<string>() },
-					{ "Bearer", Enumerable.Empty<string>() },
-				});
+			ConfigureSwagger(services);
 
-				c.OperationFilter<ResponseWithDescriptionOperationFilter>();
-			});
+			ConfigureApplicationServices(services);
 
-			// AddIdentity и AddDefaultIdentity добавляют много чего лишнего. Ссылки для сранения.
-			// https://github.com/aspnet/Identity/blob/c7276ce2f76312ddd7fccad6e399da96b9f6fae1/src/Core/IdentityServiceCollectionExtensions.cs
-			// https://github.com/aspnet/Identity/blob/c7276ce2f76312ddd7fccad6e399da96b9f6fae1/src/Identity/IdentityServiceCollectionExtensions.cs
-			// https://github.com/aspnet/Identity/blob/c7276ce2f76312ddd7fccad6e399da96b9f6fae1/src/UI/IdentityServiceCollectionUIExtensions.cs#L49
-			services.AddIdentityCore<ApplicationUser>()
-				.AddEntityFrameworkStores<ApplicationDbContext>();
-
-			// DI
-
-			services.AddSingleton<AuthSettings>(x => AuthSettings);
-
-			services.AddScoped<ITokenGenerator, TokenGenerator>();
-			services.AddScoped<IRepository<ApplicationUser, string>, EfRepository<ApplicationUser, string>>();
-			services.AddScoped<IRepositoryWithAccessCheck<Bank>, EfBanksRepository>();
-			services.AddScoped<IRepositoryWithAccessCheck<Category>, EfCategoriesRepository>();
-			services.AddScoped<IRepositoryWithAccessCheck<BankAccount>, EfBankAccountsRepository>();
-			services.AddScoped<IRepositoryWithAccessCheck<Rule>, EfRulesRepository>();
-			services.AddScoped<IRepositoryWithAccessCheck<Transaction>, EfTransactionsRepository>();
-			services.AddScoped<IRefreshTokensRepository, EfRefreshTokensRepository>();
-			services.AddScoped<ITokenGenerator, TokenGenerator>();
-			services.AddScoped<AccountsService>();
-			services.AddScoped<BanksService>();
-			services.AddScoped<RulesService>();
-			services.AddScoped<UsersService>();
-			services.AddScoped<CategoriesService>();
-			services.AddScoped<TokenService>();
-			services.AddScoped<TransactionsService>();
-			services.AddScoped<DataAccessLayer>();
-			services.AddScoped<ImportService>();
+			ConfigurePolicies(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -187,6 +85,137 @@ namespace SlidFinance.WebApi
 			});
 
 			app.UseMvc();
+		}
+
+		private void ConfigureAuthorization(IServiceCollection services)
+		{
+			AuthSettings authSettings;
+
+			if (CurrentEnvironment.IsDevelopment())
+			{
+				authSettings = Configuration
+					.GetSection("Security")
+					.GetSection("Token")
+					.Get<AuthSettings>();
+			}
+			else
+			{
+				authSettings = new AuthSettings
+				{
+					Audience = Environment.GetEnvironmentVariable("TOKEN_AUDIENCE"),
+					Issuer = Environment.GetEnvironmentVariable("TOKEN_ISSUER"),
+					Key = Environment.GetEnvironmentVariable("TOKEN_KEY"),
+					LifetimeMinutes = Convert.ToInt32(Environment.GetEnvironmentVariable("TOKEN_LIFETIME_MINUTES"))
+				};
+			}
+
+			services
+				.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.RequireHttpsMetadata = false;
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						// Укзывает, будет ли проверяться издатель при проверке токена
+						ValidateIssuer = false,
+						// Строка, представляющая издателя
+						ValidIssuer = authSettings.Issuer,
+
+						// Будет ли проверяться потребитель токена
+						ValidateAudience = false,
+						// Установка потребителя токена
+						ValidAudience = authSettings.Audience,
+						// будет ли валидироваться время существования
+						ValidateLifetime = true,
+
+						// установка ключа безопасности
+						IssuerSigningKey = authSettings.GetSymmetricSecurityKey(),
+						// валидация ключа безопасности
+						ValidateIssuerSigningKey = true,
+					};
+					// options.SaveToken = true;
+				});
+
+			services.AddSingleton<AuthSettings>(x => authSettings);
+
+			// AddIdentity и AddDefaultIdentity добавляют много чего лишнего. Ссылки для сранения.
+			// https://github.com/aspnet/Identity/blob/c7276ce2f76312ddd7fccad6e399da96b9f6fae1/src/Core/IdentityServiceCollectionExtensions.cs
+			// https://github.com/aspnet/Identity/blob/c7276ce2f76312ddd7fccad6e399da96b9f6fae1/src/Identity/IdentityServiceCollectionExtensions.cs
+			// https://github.com/aspnet/Identity/blob/c7276ce2f76312ddd7fccad6e399da96b9f6fae1/src/UI/IdentityServiceCollectionUIExtensions.cs#L49
+			services.AddIdentityCore<ApplicationUser>()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+		}
+
+		private void ConfigureAutoMapper(IServiceCollection services)
+		{
+			services.AddScoped(provider => new MapperConfiguration(cfg =>
+			{
+				cfg.AddProfile(new MappingProfile(provider.GetService<ApplicationDbContext>()));
+			}).CreateMapper());
+		}
+
+		private void ConfigureDataAccess(IServiceCollection services)
+		{
+			services.AddEntityFrameworkNpgsql()
+				.AddDbContext<ApplicationDbContext>(options => options
+					.UseLazyLoadingProxies()
+					.UseNpgsql(ConnectionStringFactory.Get()))
+				.BuildServiceProvider();
+
+			services.AddScoped<IRepository<ApplicationUser, string>, EfRepository<ApplicationUser, string>>();
+			services.AddScoped<IRepositoryWithAccessCheck<Bank>, EfBanksRepository>();
+			services.AddScoped<IRepositoryWithAccessCheck<Category>, EfCategoriesRepository>();
+			services.AddScoped<IRepositoryWithAccessCheck<BankAccount>, EfBankAccountsRepository>();
+			services.AddScoped<IRepositoryWithAccessCheck<Rule>, EfRulesRepository>();
+			services.AddScoped<IRepositoryWithAccessCheck<Transaction>, EfTransactionsRepository>();
+			services.AddScoped<IRefreshTokensRepository, EfRefreshTokensRepository>();
+
+			services.AddScoped<DataAccessLayer>();
+		}
+
+		private void ConfigureSwagger(IServiceCollection services)
+		{
+			// Register the Swagger generator, defining 1 or more Swagger documents
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info { Title = "SlidFinance", Version = "v1" });
+				c.AddSecurityDefinition("Oauth2", new OAuth2Scheme
+				{
+					Type = "oauth2",
+					Flow = "implicit",
+					TokenUrl = "/api/v1/tokens"
+				});
+				c.AddSecurityDefinition("Bearer", new ApiKeyScheme { In = "header", Description = "Please enter JWT with Bearer into field", Name = "Authorization", Type = "apiKey" });
+				c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+					{ "Oauth2", Enumerable.Empty<string>() },
+					{ "Bearer", Enumerable.Empty<string>() },
+				});
+
+				c.OperationFilter<ResponseWithDescriptionOperationFilter>();
+			});
+		}
+
+		private void ConfigureApplicationServices(IServiceCollection services)
+		{
+			services.AddScoped<ITokenGenerator, TokenGenerator>();
+			services.AddScoped<AccountsService>();
+			services.AddScoped<BanksService>();
+			services.AddScoped<RulesService>();
+			services.AddScoped<UsersService>();
+			services.AddScoped<CategoriesService>();
+			services.AddScoped<TokenService>();
+			services.AddScoped<TransactionsService>();
+			services.AddScoped<ImportService>();
+		}
+
+		private void ConfigurePolicies(IServiceCollection services)
+		{
+			services.AddAuthorization(options =>
+			{
+				// Добавляем политики на наличие нужной роли у учётной записи.
+				options.AddPolicy(Policy.MustBeAllAccessMode, policy => policy.RequireClaim(nameof(AccessMode), AccessMode.All.ToString()));
+				options.AddPolicy(Policy.MustBeImportAccessMode, policy => policy.RequireClaim(nameof(AccessMode), AccessMode.Import.ToString()));
+			});
 		}
 	}
 }
