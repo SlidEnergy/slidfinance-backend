22c4ac21e589c6e26f35d408236a94c4169a2dff
kluchko <kluchko@lers.ru>
2019-09-27 17:20:16 +1000

    build: Обновил миграции после рефакторинга пространств имен

---
+++ SlidFinance.Infrastucture/Migrations/20190927071840_RefactorNamespaces.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SlidFinance.WebApi.Migrations
+{
+    public partial class RefactorNamespaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
