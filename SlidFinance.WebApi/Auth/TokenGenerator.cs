3491d313eac2866281289265d4834bfe7b01c746
kluchko <kluchko@lers.ru>
2020-08-12 17:43:31 +1000

    refactor: уменьшил размер токена доступа

--- SlidFinance.WebApi/Auth/TokenGenerator.cs
+++ SlidFinance.WebApi/Auth/TokenGenerator.cs
@@ -14,26 +14,26 @@ namespace SlidFinance.WebApi
 	/// Формирует AccessToken и RefreshToken, необходимые для авторизации.
 	/// </summary>
 	public class TokenGenerator : ITokenGenerator
-    {
-        private readonly AuthSettings _authSettings;
+	{
+		private readonly AuthSettings _authSettings;
 
-        public TokenGenerator(AuthSettings appSettings)
-        {
-            _authSettings = appSettings;
-        }
+		public TokenGenerator(AuthSettings appSettings)
+		{
+			_authSettings = appSettings;
+		}
 
 		/// <summary>
 		/// Формирует RefreshToken, нужный для получения нового AccessToken после его истечения
 		/// </summary>
-        public string GenerateRefreshToken()
-        {
-            var randomNumber = new byte[32];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(randomNumber);
-                return Convert.ToBase64String(randomNumber);
-            }
-        }
+		public string GenerateRefreshToken()
+		{
+			var randomNumber = new byte[32];
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(randomNumber);
+				return Convert.ToBase64String(randomNumber);
+			}
+		}
 
 		/// <summary>
 		/// Формирует AccessToken для указанного пользователя.
@@ -44,7 +44,7 @@ namespace SlidFinance.WebApi
 		/// Формирует AccessToken с указанными Claims.
 		/// </summary>
 		public string GenerateAccessToken(IEnumerable<Claim> claims)
-        {
+		{
 			var token = new JwtSecurityToken(
 				issuer: _authSettings.Issuer,
 				audience: _authSettings.Audience,
@@ -52,36 +52,36 @@ namespace SlidFinance.WebApi
 				expires: DateTime.UtcNow.AddMinutes(_authSettings.LifetimeMinutes),
 				signingCredentials: new SigningCredentials(_authSettings.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
 
-            var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+			var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
 
-            return encodedToken;
-        }
+			return encodedToken;
+		}
 
-        private IEnumerable<Claim> CreateClaims(ApplicationUser user, AccessMode accessMode)
-        {
-            return new Claim[]
-                {
-                    // Asp.net core Identity восстанавливает это значение в поле User.Identity.Name
-                    // Допустипо ClaimTypes.Name или ClaimsIdentity.DefaultNameClaimType
-                    new Claim(ClaimTypes.Name, user.Email),
+		private IEnumerable<Claim> CreateClaims(ApplicationUser user, AccessMode accessMode)
+		{
+			return new Claim[]
+				{
+					// Asp.net core Identity восстанавливает это значение в поле User.Identity.Name
+					// Допустипо ClaimTypes.Name или ClaimsIdentity.DefaultNameClaimType
+					// В startup.cs мы меняем UserNameClaimsType на "email"
+					new Claim(JwtRegisteredClaimNames.Email, user.Email),
 
-                    // JWT specification
+					// JWT specification
 
-                    // Asp.net core Identity использует claims "sub", как альтернативу ClaimTypes.NameIdentifier
-                    new Claim(JwtRegisteredClaimNames.Sub, user.Id),
-                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
-                    // _jwtOptions.IssuedAt
-                    new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.Now).ToString(), ClaimValueTypes.Integer64),
+					// Asp.net core Identity использует claims "sub", как альтернативу ClaimTypes.NameIdentifier
+					new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+					// IssuedAt
+					new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.Now).ToString(), ClaimValueTypes.Integer64),
 
 					// Application
 					new Claim(nameof(AccessMode), accessMode.ToString())
-                };
-        }
+				};
+		}
 
-        /// <returns>Date converted to seconds since Unix epoch (Jan 1, 1970, midnight UTC).</returns>
-        private long ToUnixEpochDate(DateTime date)
-            => (long)Math.Round((date.ToUniversalTime() -
-                                 new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero))
-                .TotalSeconds);
-    }
+		/// <returns>Date converted to seconds since Unix epoch (Jan 1, 1970, midnight UTC).</returns>
+		private long ToUnixEpochDate(DateTime date)
+			=> (long)Math.Round((date.ToUniversalTime() -
+								 new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero))
+				.TotalSeconds);
+	}
 }
