be7e40bd16a2662e1394dbdb1730bbd536395f28
kluchko <kluchko@lers.ru>
2020-08-31 14:03:44 +1000

    chore: добавил миграцию

---
+++ SlidFinance.Infrastucture/Migrations/20200831040249_AddSaltedgeBankAccountLink.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SlidFinance.WebApi.Migrations
+{
+    public partial class AddSaltedgeBankAccountLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SaltedgeBankAccountId",
+                table: "Accounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SaltedgeBankAccountId",
+                table: "Accounts");
+        }
+    }
+}
